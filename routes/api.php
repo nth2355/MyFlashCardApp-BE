4bed1d2426c5b364f4e74e891c18bffd118fba09
Thanh Hai <nth2355@gmail.com>
2026-04-09 19:30:09 +0700

    add task management BE

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 
 // Public auth
 Route::post('/register', [AuthController::class, 'register']);
@@ -14,5 +15,10 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', fn (Request $request) => response()->json([
         'user' => $request->user(),
     ]));
+    //task
+    Route::get('/tasks', [TaskController::class, 'index']);
+    Route::post('/tasks', [TaskController::class, 'store']);
+    Route::patch('/tasks/{id}', [TaskController::class, 'update']);
+    Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
 });
 
